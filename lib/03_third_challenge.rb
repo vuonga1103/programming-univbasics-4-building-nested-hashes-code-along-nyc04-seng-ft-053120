65c26d15da49474915b308386e20922a24082c6c
Anh Vuong <vuonga1103@yahoo.com>
2020-06-03 00:59:34 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
           :age => "53"
         },
       :matriarch => {
-          name => "Lady Montague",
+          :name => "Lady Montague",
           :age => "54"
         },
       :hero => {
